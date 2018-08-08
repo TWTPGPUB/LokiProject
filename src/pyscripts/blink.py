612aba6775d36c60fa49f928395ce1f665126a63
TWTPGPUB <cmeringolo@gmail.com>
2018-08-08 18:51:43 +0000

    Previous file not staged, recomitting

--- src/pyscripts/blink.py
+++ src/pyscripts/blink.py
@@ -4,13 +4,15 @@ import time
 
 from envirophat import leds
 
+limit = 50
 
 try:
-    while True:
+    while limit > 0:
 		leds.on()
 		time.sleep(0.1)
 		leds.off()
 		time.sleep(0.1)
+		limit=limit-1
 
 except KeyboardInterrupt:
     pass
